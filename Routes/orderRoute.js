a2e9cc5ac707605d824a51f8ea7901358311ad8e
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-04-20 13:49:49 +0300

    added auhorization to the orderRoute

--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import orderController from "../Controllers/orderController.js";
+import verifyToken from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/", orderController.getAllOrders);
 router.get("/:id", orderController.getOrderById);
-router.post("/", orderController.addOrder);
-router.put("/edit/:id", orderController.editOrder);
-router.delete("/delete/:id", orderController.deleteOrder);
+router.post("/", verifyToken, orderController.addOrder);
+router.put("/edit/:id", verifyToken, orderController.editOrder);
+router.delete("/delete/:id", verifyToken, orderController.deleteOrder);
 
 export default router;
